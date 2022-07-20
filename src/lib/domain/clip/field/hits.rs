d280d0da18050f5bbeb04b0eabc89e0bc89b2118
Clupai8o0 <clupaio4@gmail.com>
2022-07-20 16:52:07 +0300

    Domain Implementation - Content & Hits

---
+++ src/lib/domain/clip/field/hits.rs
@@ -0,0 +1,15 @@
+use derive_more::Constructor;
+use serde::{Deserialize, Serialize};
+
+#[derive(Clone, Constructor, Debug, Deserialize, Serialize)]
+pub struct Hits(u64);
+
+impl Hits {
+  // pub fn new(data: u64) -> Self {
+  //   Self(data)
+  // }
+
+  pub fn into_inner(self) -> u64 {
+    self.0
+  }
+}
