88353414bd9ddbb6ef97f3f82ea89ef0cae493ea
Clupai8o0 <clupaio4@gmail.com>
2022-07-21 11:37:05 +0300

    Service Layer - New & Update Clip

--- src/lib/service/action.rs
+++ src/lib/service/action.rs
@@ -17,3 +17,11 @@ pub async fn get_clip(req: ask::GetClip, pool: &DatabasePool) -> Result<Clip, Se
     Ok(clip)
   }
 }
+
+pub async fn new_clip(req: ask::NewClip, pool: &DatabasePool) -> Result<Clip, ServiceError> {
+  Ok(query::new_clip(req, pool).await?.try_into()?)
+}
+
+pub async fn update_clip(req: ask::UpdateClip, pool: &DatabasePool) -> Result<Clip, ServiceError> {
+  Ok(query::update_clip(req, pool).await?.try_into()?)
+}
