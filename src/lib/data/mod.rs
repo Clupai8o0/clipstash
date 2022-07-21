e7656c4ae2fdcfcccc60fdeefed7c32ce917c923
Clupai8o0 <clupaio4@gmail.com>
2022-07-21 07:57:16 +0300

    Database Type Aliases (Error)

--- src/lib/data/mod.rs
+++ src/lib/data/mod.rs
@@ -2,6 +2,20 @@ use derive_more::{Display, From};
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 use std::str::FromStr;
+use thiserror;
+use sqlx::Database;
+
+#[derive(Debug, thiserror::Error)]
+pub enum DataError {
+  #[error("database error: {0}")]
+  Database(#[from] sqlx::Error)
+}
+
+pub type AppDatabase = Database<Sqlite>;
+pub type DatabasePool = sqlx::sqlite::SqlitePool;
+pub type Transaction<'t> = sqlx::Transaction<'t, Sqlite>;
+pub type AppDatabaseRow = sqlx::sqlite::SqliteRow;
+pub type AppQueryResult = sqlx::sqlite::SqliteQueryResult;
 
 #[derive(Clone, Debug, From, Display, Deserialize, Serialize)]
 pub struct DbId(Uuid);
