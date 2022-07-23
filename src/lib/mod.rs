d1af405add7e110bcb0828d4b36b837fe2ad7da4
Clupai8o0 <clupaio4@gmail.com>
2022-07-23 08:15:10 +0300

    rocket configuration

--- src/lib/mod.rs
+++ src/lib/mod.rs
@@ -9,3 +9,24 @@ pub use domain::clip::{Clip, ClipError};
 pub use domain::time::Time;
 pub use data::DataError;
 pub use service::ServiceError;
+
+use data::AppDatabase;
+use rocket::fs::FileServer;
+use rocket::{Build, Rocket};
+use web::{renderer::Renderer};
+
+// server
+pub fn rocket(config: RocketConfig) -> Rocket<Build> {
+  rocket::build()
+    .manage::<AppDatabase>(config.database)
+    .manage::<Renderer>(config.renderer)
+    .mount("/", web::http::routes())
+    .mount("/static", FileServer::from("static"))
+    .register("/", web::http::catcher::catchers())
+}
+
+// server config
+pub struct RocketConfig {
+  pub renderer: Renderer<'static>,
+  pub database: AppDatabase,
+}
